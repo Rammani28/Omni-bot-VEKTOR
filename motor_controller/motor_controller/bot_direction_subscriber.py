9a2d0778be244baecf3aa999ff39014e808820ba
rammani <rammani@pi.com>
2025-02-08 16:35:38 +0000

    the driving part works fine

--- motor_controller/motor_controller/bot_direction_subscriber.py
+++ motor_controller/motor_controller/bot_direction_subscriber.py
@@ -3,56 +3,58 @@ from rclpy.node import Node
 from std_msgs.msg import String
 import serial
 from motor_controller.utils import target_wheel_rpm
-from motor_controller.motor import Motor
-from time import sleep
+from motor_controller.motor2 import Motor
 
 ser = serial.Serial("/dev/serial0", baudrate=115200, timeout=0)
 
+
 class PwmSubscriberNode(Node):
-    """CREATED YOUR OWN PROBLEM AND CRY"""
     def __init__(self):
         super().__init__('bot_direction_subscriber') # name of node
-        print("subscriber initialized")
+        print("subscriber initialized, !!!!!!!RUN CODE ON THONNY!!!!!!!!!!")
+
         self.subscription = self.create_subscription(msg_type=String, topic='/bot_direction', callback=self.listener_callback, qos_profile=10) # subscribe topic /led_control using String type, queue size is 10
         self.motor1 = Motor(5, 6, name='motor1') # physical 29, 31 
         self.motor2 = Motor(27, 17, 'motor2')  # physical 11, 13
         self.motor3 = Motor(23, 24, 'motor3') # physical 16 18
 
+    def stop_motors(self):
+        self.motor1.stop()
+        self.motor2.stop()
+        self.motor3.stop()
 
     def listener_callback(self, msg):
         theta = float(msg.data.split(',')[0]) 
-        wbz = float(msg.data.split(',')[-1])
+        wbz = float(msg.data.split(',')[1])
         # print(f"theta:{theta}, omega:{wbz}")
-        
-        if theta == 501:
-            wbz = 0
-            theta = 0
-            self.motor1.stop()
-            self.motor2.stop()
-            self.motor3.stop()
-            # print("no input from joystick....")
+        start = True
+        if theta == 501 and wbz == 501:
+            start = False
+            self.stop_motors()
             return
 
-        if theta != 501:
-            u1, u2, u3 = target_wheel_rpm(theta, 0) # -100 to 100 for comparable pwm
-            # rpm1, rpm2, rpm3 = [None, None, None]
-            # print(f"u1={u1},\t u2={u2},\t u3={u3}")
+        if start:
+            
             rpms_pico = ser.readline().decode().strip()  # Read and decode
+            print(rpms_pico)
 
             if rpms_pico:
-                # print("\n")
-                # self.get_logger().info(f"Received theta:{theta}, wbz:{wbz}")
-                rpm1, rpm2, rpm3 = [float(rpm) for rpm in rpms_pico.split(',')] #is in pwm value 
+                
+                print("angle", theta, wbz)
+                try:
+                    rpm1, rpm2, rpm3 = [float(rpm) for rpm in rpms_pico.split(',')] #is in pwm value 
+                    print(f"r1:{rpm1:.2f}\t r2:{rpm2:.2f}\t r3:{rpm3:.2f}")
+                except ValueError:
+                    print("\n\nmissed an rpm measurement\n\n")
+                    # self.stop_motors()
+                    return
 
-                # self.get_logger().info(f"theta:{theta}, wbz:{wbz}")
-                # self.get_logger().info(f"r1={rpm1},\t r2={rpm2},\t r3={rpm3}")
-                # self.get_logger().info(f"u1:{u1:.1f}")
-                # print(f"r1={rpm1:.2f},\t r2={rpm2:.2f},\t r3={rpm3:.2f}")
-                print(f"angle:{theta}, wbz:{wbz}")
-                print(f"u1:{u1:.2f} \t u2:{u2:.2f} \t u3:{u3:.2f}")
+                u1, u2, u3 = target_wheel_rpm(theta, wbz)
+                print(f"u1:{u1:.2f}\t u2:{u2:.2f}\t u3:{u3:.2f}")
                 self.motor1.rotate(u1, rpm1)
                 self.motor2.rotate(u2, rpm2)
                 self.motor3.rotate(u3, rpm3)
+                print()
 
 
     def __del__(self):  # destructor for motor class, How does it work?
